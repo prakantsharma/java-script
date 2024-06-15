a742c49b6eb410f32ad012e60e6bf3f44915fc03
Prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2024-06-15 16:17:31 +0000

    Varibales

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=14453;
+let accountEmail="prakantsharma38@gmail.com";
+var accountPassword="12345";
+accountcity="jaipur";
+// accountId=2; not allowed 
+let accountstate;
+console.log(accountId);
+accountEmail="pksharma";
+accountPassword="0000";
+accountcity="bengaluru";
+console.table([accountEmail,accountPassword,accountcity,accountstate]);
+/*
+Don't use var because of issue in block scope and functional scope;
+*/
