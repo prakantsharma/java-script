26cd73dca9abe5c624d6b5b2460d9b6ca68cc0e1
Prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2024-06-18 08:50:40 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,73 @@
+// let score=123
+// console.log("type of score is",typeof score);
+// // Another method
+// console.log(typeof(score));
+
+// let age="19abc";
+// let valueinage=Number(age);
+// console.log(valueinage);
+// console.log(typeof valueinage);
+// let x=null;
+// console.log(x);
+
+// let y=undefined;
+// console.log(y)
+// let istrue=Number(true);
+// let isnot=Boolean(0);
+// console.log(istrue)
+// console.log(isnot);
+// // "33"=number main conver ho jata hai 
+// //"33abc"Nan-not a number
+// let name="prakant"
+// let boolname=Boolean(name)
+// console.log(boolname);
+
+// let anum=33
+// let strnumber=String(anum)
+// console.log(strnumber);
+// console.log(typeof strnumber);
+
+// // operations;;
+ let value=3
+let negvalue= -value
+ console.log(negvalue)
+ console.log(typeof(negvalue));
+
+ let str_1="hello";
+ let str_2="sharma";
+ let fullname=str_1+str_2
+console.log(fullname);
+
+console.log("1"+2);
+console.log("1"+2+2);// pehale string to sabhi string main ho jayega
+
+console.log(1+2+"2");// jo pehale aata hai usi main ho jata hai
+console.log(1+2+"3"+2);//332
+console.log(+true)// gandi baat 
+let num1,num2,num3
+num1=num2=num3=2+2;// readatibilty kam hai iski ,isliye mat karna
+console.table([num1,num2,num3]);
+let gamecounter=100
+//gamecounter ++;
+++ gamecounter;
+console.log(gamecounter)
+//
+let x=3
+const y=x++;// pehale assign kar dega tab badhayega
+console.log(`x:${x},y:${y}`)
+const z=++x;// pehale badha dega tab assign karega
+console.log(`x:${x},z:${z}`);
+// ab dekh aur majedar chij
+let a=2n;
+const b=a++;
+console.log(b)
+let q =5n;
+const c=++q ;
+console.log(c)
+
+
+
+
+
+
+
