53977273b640d13f533572bb8e3e38e7fddda541
Prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2024-06-19 22:18:04 +0000

    strings

---
+++ strings/01_strings.js
@@ -0,0 +1,10 @@
+let name="Prakant sharma"
+let nickname="sakhi"
+console.log(name+nickname);// bhai ye old hai new dekh
+
+console.log(`name:${name} and nickname:${nickname}`)
+
+const gamename= new String('lalala')
+console.log(gamename)
+console.log(gamename[0])
+console.log(gamename.__proto__);
