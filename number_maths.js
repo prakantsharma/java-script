52ce8ef5000e5082e38a59a2a61ae0b3b119305a
Prakant_sharma <166603325+prakantsharma@users.noreply.github.com>
2024-06-22 06:49:15 +0000

    math & num

---
+++ number_maths.js
@@ -0,0 +1,23 @@
+// const score=400;
+// const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString.length);
+// const othernumber=23.897654;
+// console.log(othernumber.toPrecision(3))
+
+// const thirdnumber=1000000;
+// console.log(thirdnumber.toLocaleString('en-IN'));
+
+// maths library
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+
+console.log(Math.trunc(1.2));// return integral part of the number
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min=10;/* will give a number between this range*/
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
